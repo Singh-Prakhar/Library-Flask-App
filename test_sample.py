cc9edc53bda6f2140b6988fa0c16759ae322e96e
Prakhar Singh <prakharsingh1023@gmail.com>
2022-11-07 18:27:07 +0530

    Updated tests

--- test_sample.py
+++ test_sample.py
@@ -7,13 +7,19 @@ os.environ["LIBRARY_TABLE"] = "Library-table-dev"
 def test_insert_item():
     dnb = DynamodbUtil()
     response, response_serial_id = dnb.insert_item(book_name="test_book_name", author_name="test_author_name")
+    response_check = dnb.get_item(response_serial_id)
+
     assert response_serial_id is not None
+    assert response_check['Item']['authorName']['S'] == 'test_author_name', response_check
+    assert response_check['Item']['bookName']['S'] == 'test_book_name', response_check
 
 
 def test_get_item():
     dnb = DynamodbUtil()
-    response = dnb.get_item("test_serial_id")
-    assert response is not None
+    response, response_serial_id = dnb.insert_item(book_name="test_book_name", author_name="test_author_name")
+    response_check = dnb.get_item(response_serial_id)
+    assert response_check is not None
+    assert response_check['Item']['authorName']['S'] == 'test_author_name', response_check
 
 
 def test_get_all_items():
@@ -25,25 +31,40 @@ def test_get_all_items():
 
 def test_update_item_book_name_author_name():
     dnb = DynamodbUtil()
-    response = dnb.update_item("test_serial_id", "test_book_name", "test_author_name")
-    assert response == "Information updated. New book name: " + "test_book_name" + "New author name"\
-           + "test_author_name", response
+    response, response_serial_id = dnb.insert_item(book_name="test_book_name", author_name="test_author_name")
+    updated_response = dnb.update_item(response_serial_id, "new_test_book_name", "new_test_author_name")
+    response_check = dnb.get_item(response_serial_id)
+    assert updated_response == "Information updated. New book name: " + "new_test_book_name" + "New author name"\
+           + "new_test_author_name", updated_response
+    assert response_check['Item']['authorName']['S'] == 'new_test_author_name', response_check
+    assert response_check['Item']['bookName']['S'] == 'new_test_book_name', response_check
 
 
 def test_update_item_book_name():
     dnb = DynamodbUtil()
-    response = dnb.update_item("test_serial_id", book_name="test_book_name")
-    assert response == "New book name " + "test_book_name", response
+    response, response_serial_id = dnb.insert_item(book_name="test_book_name", author_name="test_author_name")
+    updated_response = dnb.update_item(response_serial_id, book_name="new_test_book_name")
+    response_check = dnb.get_item(response_serial_id)
+
+    assert updated_response == "New book name " + "new_test_book_name", updated_response
+    assert response_check['Item']['authorName']['S'] == 'test_author_name', response_check
+    assert response_check['Item']['bookName']['S'] == 'new_test_book_name', response_check
 
 
 def test_update_item_author_name():
     dnb = DynamodbUtil()
-    response = dnb.update_item("test_serial_id", author_name="test_author_name")
-    assert response == "New author name " + "test_author_name"
+    response, response_serial_id = dnb.insert_item(book_name="test_book_name", author_name="test_author_name")
+    updated_response = dnb.update_item(response_serial_id, author_name="new_test_author_name")
+    response_check = dnb.get_item(response_serial_id)
+    assert updated_response == "New author name " + "new_test_author_name"
+    assert response_check['Item']['authorName']['S'] == 'new_test_author_name', response_check
+    assert response_check['Item']['bookName']['S'] == 'test_book_name', response_check
 
 
 def test_delete_item():
     dnb = DynamodbUtil()
-    response = dnb.delete_item("test_serial_id")
-    assert response == "Item deleted with serial id " + "test_serial_id"
-
+    response, response_serial_id = dnb.insert_item(book_name="test_book_name", author_name="test_author_name")
+    delete_response = dnb.delete_item(response_serial_id)
+    response_check = dnb.get_item(response_serial_id)
+    assert delete_response == "Item deleted with serial id " + response_serial_id
+    assert 'Item' not in response_check
